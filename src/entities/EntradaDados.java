7a0d56df7778d819047d8b331e3e3ea78dae5d08
Monique Silva <moniquesntsilva@gmail.com>
2023-12-05 00:22:32 -0300

    adiciona comandos para console

---
+++ src/entities/EntradaDados.java
@@ -0,0 +1,29 @@
+package entities;
+import java.util.HashMap;
+
+import commands.*;
+
+public class EntradaDados {
+	private HashMap<String,Object> comando = new HashMap<String,Object>();
+
+  public EntradaDados(Comando comando, String[] args) {
+    listaComandos();
+  }
+
+  // Lista de comandos possíveis
+  private void listaComandos() {
+    this.comando.put("emp", new EmprestimoComando());
+    this.comando.put("dev", new DevolverComando());
+    this.comando.put("res", new ReservaComando());
+    this.comando.put("obs", new ObservadorComando());
+    this.comando.put("liv", new LivroComando());
+    this.comando.put("usu", new UsuarioComando());
+    this.comando.put("ntf", new NotificacaoComando());
+  }
+
+  // Executar comando
+  public void execute(String[] args) {
+    Comando cmd = (Comando) this.comando.get(args[0]);
+    cmd.executar(args);
+  }
+}
